4d3650e34e74dbb2fec1fe5989e19ff3cffaa302
acsaralegui <aaron.saralegui@gmail.com>
2022-02-14 22:22:06 -0700

    Cards and Footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
-import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 
 function App() {
@@ -12,7 +15,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
-          
+          <Route path='/Services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />         
         </Switch>
       </Router>
     </>
